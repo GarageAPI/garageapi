6e3559321e9ad13182cc8d2c380205f37b581377
VoltMX <v0ltmx@yandex.ru>
2023-10-28 20:46:10 +0300

    Add initial projects

---
+++ api/src/main/java/net/garagecloud/basical/GarageAPI.java
@@ -0,0 +1,23 @@
+package net.garagecloud.basical;
+
+import lombok.extern.log4j.Log4j2;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Log4j2
+public final class GarageAPI extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        //todo: Logic load
+    }
+
+    @Override
+    public void onEnable() {
+        //todo: Logic enable
+    }
+
+    @Override
+    public void onDisable() {
+        //todo: Logic disable
+    }
+}
